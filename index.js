bfc95e51ae9e025f0f9d11d60f8ad4c0ef852930
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-02-15 20:57:25 +0600

    solve second task

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function myName(name, surname) {
   console.log(`My name is ${name} ${surname}`);
 }
 myName("Erbol", "Alymbekov");
+
+function printVol(height, radius) {
+  let pi = 3.14;
+  let result = (pi * (radius ** 2) * height) / 3;
+  console.log(`The volume is ${result}`)
+}
+printVol(7, 10);
