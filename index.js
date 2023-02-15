3ca89c9b74e10ac9b52b4fb02c0936dded9b46bf
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-02-15 22:12:02 +0600

    solved twelfth task

--- index.js
+++ index.js
@@ -61,8 +61,15 @@ for (let i = 0; i <= 10; i++) {
 }
 
 let num = 0;
- for (let i = 0; i <= 25; i++) {
+for (let i = 0; i <= 25; i++) {
   num = num + i;
   console.log(num);
+}
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i)
+  }
+  
  }
 
